3425cc9617f1010ff91acda91d45789f426e41b3
Nineteen991 <Nineteen99@gmail.com>
2022-12-05 21:41:43 -0800

    still setting up

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/home'
+import Chat from './pages/chat'
+
 import './App.sass'
 
 export default function App() {
   return (
-    <div className='container'>
-      howdy
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={ <Home /> } />
+        <Route path='/chat' element={ <Chat /> } />
+      </Routes>
+    </Router>
   )
 }
---
+++ client/src/pages/chat/index.js
@@ -0,0 +1,7 @@
+export default function Chat() {
+  return (
+    <div className="chat">
+      Chat
+    </div>
+  )
+}
